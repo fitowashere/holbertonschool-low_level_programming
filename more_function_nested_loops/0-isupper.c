0d40bd4968e0cd532a5b675fa08e21c92621bc4c
fitowashere <fito_washere@icloud.com>
2023-06-26 14:57:10 +0000

    task 0

--- more_function_nested_loops/0-isupper.c
+++ more_function_nested_loops/0-isupper.c
@@ -2,6 +2,15 @@
 
 /**
  * isupper- gets users input
- * Return:
+ * @c: stores users input in c
+ * Return: result
  */
 
+int _isupper(int c)
+{
+
+	if((c > 64) && (c < 91))
+		return(1);
+	else 
+		return(0);
+}
