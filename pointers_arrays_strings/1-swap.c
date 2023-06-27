32513dc295dcd486ec7caebb0d66ed589d2fac70
fitowashere <fito_washere@icloud.com>
2023-06-27 12:22:47 +0000

    $c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *swap_int- gets users input
+ *@a: stores first input
+ *@b: stores second input
+ *Return: results
+ */
+
+void swap_int(int *a, int *b)
+{
+
+int c, d;
+
+c = *a;
+d = *b;
+
+*a = d;
+*b = c;
+
+
+}
