7a146c573377c0cfc0052b12c07ee84e4843b83d
fitowashere <fito_washere@icloud.com>
2023-06-27 14:06:06 +0000

    $c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *rev_string- gets users input
+ *@s: stores users input
+ *Return: results
+ */
+
+void rev_string(char *s)
+{
+	int x = 0, y = 0, z = 0;
+
+	char tmp[10];
+
+	while (s[x] != '\0')
+	{
+		_putchar(s[x]);
+		x++;
+		z++;
+		if (s[x] == '\0')
+		{
+			while (z != 0)
+			{
+				tmp[y] = s[z - 1];
+				_putchar(tmp[y]);
+				y++;
+				z--;
+
+			}
+		}
+	}
+	_putchar(10);
+}
