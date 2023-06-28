2e6ca76ce29e6d967f1727a6d72d8cf7a0a180b3
fitowashere <fito_washere@icloud.com>
2023-06-28 12:31:28 +0000

    $c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *puts_half- gets users input
+ *@str: stores users input
+ */
+
+void puts_half(char *str)
+{
+	int x = 0;
+	int y;
+
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	y = x-- /2;
+	 while (y > x)
+	 {
+		 
+		if (x-- / 2 == 0)
+		{
+			_putchar(str[x]);
+			x++;
+		}
+	 }
+	_putchar('\n');
+
+}
