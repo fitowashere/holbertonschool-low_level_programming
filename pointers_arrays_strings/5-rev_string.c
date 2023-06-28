8ad43986cbc07529ba99f46a20908d327ea4fbed
fitowashere <fito_washere@icloud.com>
2023-06-28 12:05:20 +0000

    $c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,23 +7,17 @@
 
 void rev_string(char *s)
 {
-	int x = 0, y = 0, z = 0;
-	char tmp;
+	int x = 0, z = 0;
+	char t;
 
 	while (s[x] != '\0')
 	{
 		x++;
-		z++;
-		if (s[x] == '\0')
-		{
-			while (z != 0)
-			{
-				tmp[y] = s[z - 1];
-				_putchar(tmp[y]);
-				y++;
-				z--;
-			}
-		}
 	}
-	_putchar(10);
+	for (z = 0; z < x--; z++)
+	{
+		t = s[x];
+		s[x] = s[z];
+		s[z] = t;
+	}
 }
