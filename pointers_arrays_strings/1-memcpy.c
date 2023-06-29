a19f2a45ca300274cd497b8fe958659e96973a8e
fitowashere <fito_washere@icloud.com>
2023-06-29 23:38:17 +0000

    $c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ **_memcpy- gets users input
+ *@dest: gets first input
+ *@src: gets second input
+ *@n: gets third input
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for ( i = 0; src[i] != '\0' && i > n; i++, n--)
+	{
+		if (src[i] != '\0')
+		{
+			dest[i] = src[i];
+		}
+	}
+	return [dest];
+}
