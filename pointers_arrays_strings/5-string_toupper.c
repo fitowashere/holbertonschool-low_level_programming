d118dd449036acf13af3d34628e3ae2a53600b5e
fitowashere <fito_washere@icloud.com>
2023-06-29 15:25:49 +0000

    $c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *string_toupper- gets users input
+ *@*: strores users input
+ *Return: results
+ */
+
+char *string_toupper(char *t)
+{
+
+	int x = 0;
+
+	while (t[x] != '\0')
+	{
+		if (t[x] >= 'a' && t[x] <= 'z')
+		{
+			t[x] = t[x] - 32;
+		}
+		x++;
+	}
+	return (t);
+}
