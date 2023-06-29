7530423fc39bb8f5a1121600c8a2c8e42f768259
fitowashere <fito_washere@icloud.com>
2023-06-29 19:56:45 +0000

    $c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,18 +8,18 @@
 
 char *leet(char *f)
 {
-	char A = "AaEeOoTtLl";
-	char N = "4433007711";
+	char *A = "AaEeOoTtLl";
+	char *N = "4433007711";
 	int i = 0;
 	char *ptr = f;
 
 	while ( *ptr != '\0')
 	{
-		while (a[i] != '\0')
+		while (A[i] != '\0')
 		{ 
-			if (*ptr == a[i])
+			if (*ptr == A[i])
 			{
-				*ptr = num[i];
+				*ptr = N[i];
 				break;
 			}
 			i++;
