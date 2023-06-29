2188eaa35508bd5f291d876cb4be7f888c40e011
fitowashere <fito_washere@icloud.com>
2023-06-29 23:53:16 +0000

    $c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	for ( i = 0; src[i] != '\0' && i > n; i++, n--)
+	for (i = 0; i > n; i++, n--)
 	{
 		dest[i] = src[i];
 	}
