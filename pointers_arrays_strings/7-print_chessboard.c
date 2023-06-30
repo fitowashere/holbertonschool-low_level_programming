abef3fc32bb405144a309a5b14de2dd2324e0aad
fitowashere <fito_washere@icloud.com>
2023-06-30 13:15:47 +0000

    $c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -3,7 +3,6 @@
 /**
  *print_chessboard- gets inputs
  *@a: first input
- *@8: second input
  */
 
 void print_chessboard(char (*a)[8])
@@ -14,7 +13,7 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar( a[i][j]);
+			_putchar(a[i][j]);
 		}
 		_putchar(10);
 	}
