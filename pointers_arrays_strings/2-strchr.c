5901d1b7d95dcdb602404150af3494d8e8bfd86d
fitowashere <fito_washere@icloud.com>
2023-06-30 00:17:00 +0000

    $c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ **_strchr- gets users input
+ *@s: stores first input
+ *@c: stores second input
+ *Return: result
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	int i = 0;
+	while (s[i] != NULL)
+	{
+		if (s[i] == c)
+			return [c];
+		else
+			return NULL;
+
+
+	}
+}
