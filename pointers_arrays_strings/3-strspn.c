6864c21d0222f3ce856d160138ac87822912129b
fitowashere <fito_washere@icloud.com>
2023-06-30 02:00:49 +0000

    task 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,23 +10,18 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0;
+	int x;
 
-	while (*s != '\0' && *s != ' ')
+	while (*accept != '\0')
 	{
-		while (*accept != '\0')
+		for (x = 0; s[x] != '\0'; x++)
 		{
-			if (*s == *accept)
+			if (s[x] == *accept)
 			{
 				i++;
-				break;
 			}
-			accept++;
-		}
-		/*	if (*accept == '\0')
-			{
-				break;
-			}*/
-			s++;
+		}	
+		accept++;
 	}
 	return (i);
 }
