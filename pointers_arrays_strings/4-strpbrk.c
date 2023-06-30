a3cc7371d32f29470e446eb771c447b904faa84e
fitowashere <fito_washere@icloud.com>
2023-06-30 03:07:17 +0000

    $c

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_strpbrk: gets users input
+ *@s: stores first input
+ *@accept: second input
+ *Return: results
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+	while (*s != '\0')
+	{
+		while (*accept != '0')
+		{
+			if (*s == *accept)
+			{
+				return (s);
+			}
+			accept++;
+		}
+		s++
+	}
+
+	return ('\0');
+}
