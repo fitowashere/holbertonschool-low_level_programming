9f681a8bf301bae8de2bb58efa806cc3d909cc31
fitowashere <fito_washere@icloud.com>
2023-07-01 00:55:31 +0000

    $c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -12,10 +12,13 @@ void print_diagsums(int *a, int size)
 
 	int i, i1, i2;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (size * size); i = (i + size + 1))
 	{
-		i1 += *(a + i * size + i);
-		i2 += *(a + i * size + (size - 1 - i));
+		i1 = i1 + a[i];
+	}
+	for (i = size - 1; i < (size * size - 1); i = (i + size - 1))
+	{
+		i2 = i2 + a[i];
 	}
 	printf("%d, %d\n", i1, i2);
 }
