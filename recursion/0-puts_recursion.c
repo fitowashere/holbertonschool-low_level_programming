1fd597dbe6b083a6313f126dafe0c1f3a45295f0
fitowashere <fito_washere@icloud.com>
2023-07-04 20:12:31 +0000

    $c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- *_puts_recursion- gets users input
- *@s: Stores users input.
- *Return: nothing.
+ * _puts_recursion - gets users input
+ * @s: gets input
  */
 
 void _puts_recursion(char *s)
 {
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
 
 	if (*s == '\0')
 	{
 		_putchar(10);
 		return;
-	}
-
-	else if (*s != '\0')
-	{
-	_putchar(*s);
-	s++;
 	}
 
 	_puts_recursion(s);
