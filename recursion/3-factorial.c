612501423868c407ce429e4bd5246a90cc52395b
fitowashere <fito_washere@icloud.com>
2023-07-04 21:07:34 +0000

    $c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *factorial- gets input
+ *@n: stores input in variable.
+ *Return: results
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+
+}
