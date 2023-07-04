c862de0ff7c0c38df70b2735dc4df43c6e069ff2
fitowashere <fito_washere@icloud.com>
2023-07-04 19:01:02 +0000

    $c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion: gets users input
+ *@s: stores input in variable
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+	return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(s);
+
+}
