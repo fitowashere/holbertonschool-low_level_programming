0aa1d147b93b1d77e964906ef750059eb06ac012
fitowashere <fito_washere@icloud.com>
2023-07-05 15:41:35 +0000

    $c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main- gets input
+ *@argc: stores first input
+ *@argv: stores second input
+ *Return: results
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; *argv; i++, argv++)
+		;
+		printf("%d\n", argc);
+
+	return (0);
+}
