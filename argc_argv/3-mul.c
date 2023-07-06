5403a4c1513577db4a968150f300c6bb31a94b1e
fitowashere <fito_washere@icloud.com>
2023-07-06 12:09:47 +0000

    $c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main- gets input
+ * @argc: stores int input in variable
+ * @argv: stores string input in variable
+ * Return: results
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, mul;
+
+    if (argc != 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+
+	 num1 = atoi(argv[1]);
+	 num2 = atoi(argv[2]);
+	 mul = num1 * num2;
+
+	printf("%d\n", mul);
+	return (0);
+}
+
