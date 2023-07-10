a63c27593401924e4316e7b2b50b1cfea97914fd
fitowashere <fito_washere@icloud.com>
2023-07-10 15:13:04 +0000

    $c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *creat_array- gets users input
+ *@size: gets first input
+ *@c gets second input
+ *Return: results
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int ui;
+	char *array = malloc(size * sizeof(char));
+
+    if (size == 0)
+   	{
+        return NULL;
+    }
+
+    if (array == NULL)
+   	{
+        return NULL;
+    }
+
+    for (ui =  0; ui < size; ui++)
+   	{
+        array[ui] = c;
+	}
+
+    return array;
+}
