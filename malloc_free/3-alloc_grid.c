a70a19ae0adcfc8570a8db08e44defb1e0ae944c
fitowashere <fito_washere@icloud.com>
2023-07-12 13:51:31 +0000

    $c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -27,14 +27,16 @@ int **alloc_grid(int width, int height)
 		grid[i] = (int *)malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
-			for (j = 0; j < i; j++) {
+			for (j = 0; j < i; j++)
+		   	{
 				free(grid[j]);
 			}
 			free(grid);
 			return NULL;
 		}
 
-		for (j = 0; j < width; j++) {
+		for (j = 0; j < width; j++)
+	   	{
 			grid[i][j] = 0;
 		}
 	}
@@ -42,4 +44,3 @@ int **alloc_grid(int width, int height)
 	return grid;
 }
 
-}
