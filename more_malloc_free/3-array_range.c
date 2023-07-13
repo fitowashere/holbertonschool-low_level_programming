fbdcfc52da0244e31df02a4e7422a6ce17c6df74
fitowashere <fito_washere@icloud.com>
2023-07-13 16:48:31 +0000

    $c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -17,11 +17,11 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	range = malloc((max - min + 1) * sizeof(int));
+	range = malloc(((max - min) + 1) * sizeof(int));
 
 	if (range == NULL)
 		return (NULL);
-	for (i = 0; i < max - min; i++)
+	for (i = 0; i < (max - min) + 1; i++)
 	{
 		range[i] = min + i;
 	}
