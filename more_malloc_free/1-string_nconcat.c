9444cc7ac7848b80b916d5604b0526c170897937
fitowashere <fito_washere@icloud.com>
2023-07-13 13:41:05 +0000

    $c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > strlen(s2))
 		n = strlen(s2);
 
-	s3 = malloc(strlen(s1) + n + 1);
+	s3 = malloc(strlen(s1) + n);
 
 	if (s3 == NULL)
 		return (NULL);
@@ -32,10 +32,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s3[i] = s1[i];
 	}
-		for (x = 0; x <= n; x++, i++)
-		{
-			s3[i] = s2[x];
-		}
-
+	for (x = 0; x < n; x++, i++)
+	{
+		s3[i] = s2[x];
+	}
+	s3[i] = '\0';
 	return (s3);
 }
