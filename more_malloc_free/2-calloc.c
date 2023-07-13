d962e3525d71f62c3b0baae7e838797e969a3652
fitowashere <fito_washere@icloud.com>
2023-07-13 15:49:15 +0000

    $c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *_calloc- gets input
+ *@nmemb: stores first input
+ *@size: stores second input
+ *Return: results
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+	unsigned int i;
+	char *array;
+
+	array = (char *)malloc(nmemb * size);
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = 0;
+	}
+
+	return (array);
+}
