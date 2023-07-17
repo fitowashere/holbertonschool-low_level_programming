d6a473572af2d2d1fb4c0065fb4aee9217f3791a
fitowashere <fito_washere@icloud.com>
2023-07-17 14:10:12 +0000

    $c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -7,7 +7,6 @@
  *@name: stores second variable
  *@age: stores third variable
  *@owner: stores fourh variale
- *Return: results
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
