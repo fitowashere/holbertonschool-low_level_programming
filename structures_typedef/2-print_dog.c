f691d7e7f09ff5419cd62ed438903e826bde587b
fitowashere <fito_washere@icloud.com>
2023-07-18 12:44:07 +0000

    $c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,13 +11,25 @@ void print_dog(struct dog *d)
 {
 
 	if (d == NULL)
-	{
 		return;
-	}
 
-	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+
+	else
+		printf("Name: (nil)\n");
+
+	if(d->age > 0)
+		printf("Age: %f\n", d->age);
+
+	else
+		printf("Age: (nil)\n");
+
+	if (d->owner != NULL)
+		printf("Owner: %s\n", d->owner);
+
+	else
+		printf("Owner: (nil)\n");
 }
 
 
