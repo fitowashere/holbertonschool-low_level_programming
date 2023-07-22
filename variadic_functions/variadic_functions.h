11506046a24970fe810d6e16ea86798d33cefd76
fitowashere <fito_washere@icloud.com>
2023-07-22 03:00:16 +0000

    $c

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,6 +5,17 @@
 #include <string.h>
 #include <stdarg.h>
 
+/**
+ * struct typ - types of data and their function
+ * @ltr: the type of data
+ * @type: pointer to function for type
+ */
+typedef struct typ
+{
+	char *ltr;
+	void (*type)();
+} typ;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
