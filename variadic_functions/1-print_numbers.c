41e03e1e85ea8e6c08507cee9b8eebc5f5cf1256
fitowashere <fito_washere@icloud.com>
2023-07-22 01:48:17 +0000

    $c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -8,17 +8,18 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i, count;
+	unsigned int i;
 	va_list num;
+	int count;
 
 	va_start(num, n);
 
 	for (i = 0; i < n; i++)
 	{
-		num = va_arg(num, int);
-		printf("%d", num);
+		count = va_arg(num, int);
+		printf("%d", count);
 		if (i < n && separator != NULL)
-			printf("%s" separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(num);
