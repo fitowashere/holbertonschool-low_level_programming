7640dd2973869562c3e12672dec266103b8496bc
fitowashere <fito_washere@icloud.com>
2023-07-22 01:33:28 +0000

    $c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,27 +11,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i, count;
 	va_list num;
 
-	/**
-	 * trying something new with the cast to NULL lets see if it works it can also
-	 *be done by comparing it to negative in this case since its
-	 *an unsigned int and it cant have negative entries
-	 */
-
-	if (separator != NULL && n != (unsigned int *)NULL)
-	{
-
-		va_start(num, n);
-
-	}
+	va_start(num, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum = va_arg(sum, int);
-		printf("%d", sum);
+		num  variadic_function.h = va_arg(num, int);
+		printf("%d", num);
 		if (i < n && separator != NULL)
 			printf("%s" separator);
 	}
-	printf(("\n");
-			va_end(sum);
+	printf("\n");
+	va_end(num);
 
-			}
+}
