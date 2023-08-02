07e0cf8e77c734a2e0f8bd90b8edd74fa4d7b15d
fitowashere <fito_washere@icloud.com>
2023-08-02 09:25:09 -0400

    task 0

--- double_linked_lists/0-print_dlistint.c
+++ double_linked_lists/0-print_dlistint.c
@@ -1,31 +1,36 @@
 #include "lists.h"
 
 /** 
-*print_dlistint- gets input
-*@h: stores input
-*Return: results
-*/
+ *print_dlistint- gets input
+ *@h: stores input
+ *Return: results
+ */
 
 size_t print_dlistint(const dlistint_t *h)
-{
-return (printf("%d\n", h->n));
-
+{size_t count = 0;
+	while(h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return(count);
 }
 /* 
-size_t pcount = 0, count = 0;
+   size_t pcount = 0, count = 0;
 
-if (h->next == NULL || h->prev == NULL)
-return (printf("[0] (nil)"));
+   if (h->next == NULL || h->prev == NULL)
+   return (printf("[0] (nil)"));
 
-while (h->next != NULL)
-{
-count++;
-h = h->next;
-}
+   while (h->next != NULL)
+   {
+   count++;
+   h = h->next;
+   }
 
-while (h->prev != NULL)
-{
-pcount++;
-h = h->prev;
-}
-*/
+   while (h->prev != NULL)
+   {
+   pcount++;
+   h = h->prev;
+   }
+   */
