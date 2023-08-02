804598497625f7e80f7d7cb83a9e1ac66e55afe7
fitowashere <fito_washere@icloud.com>
2023-08-02 09:39:02 -0400

    fixing betty

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,36 +1,20 @@
 #include "lists.h"
 
-/** 
+/**
  *print_dlistint- gets input
  *@h: stores input
  *Return: results
  */
 
 size_t print_dlistint(const dlistint_t *h)
-{size_t count = 0;
-	while(h != NULL)
+{
+	size_t count = 0;
+
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
-	return(count);
+	return (count);
 }
-/* 
-   size_t pcount = 0, count = 0;
-
-   if (h->next == NULL || h->prev == NULL)
-   return (printf("[0] (nil)"));
-
-   while (h->next != NULL)
-   {
-   count++;
-   h = h->next;
-   }
-
-   while (h->prev != NULL)
-   {
-   pcount++;
-   h = h->prev;
-   }
-   */
