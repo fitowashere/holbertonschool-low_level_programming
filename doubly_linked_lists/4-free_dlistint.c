3e37288ba767e4aff375e5960d6ba6cc1bd1613f
fitowashere <fito_washere@icloud.com>
2023-08-04 10:30:23 -0400

    task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/*
+ *free_dlistint- gets input
+ *@head: stores input
+ *Return: results
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	while (head != NULL)
+	{
+		free(head->n);
+		free(head);
+		head = head->next;
+	}
+
+}
