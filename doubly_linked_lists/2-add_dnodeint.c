ce645426523aed197b3b464652e76b7e29bc7425
fitowashere <fito_washere@icloud.com>
2023-08-04 08:33:05 -0400

    hot fix

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,29 +1,27 @@
 #include "lists.h"
 
 /**
-*add_dnodeint- gets input
-*@head: stros input
-*@n: strones int input
-*Return: results
-*/
+ *add_dnodeint- gets input
+ *@head: stros input
+ *@n: strones int input
+ *Return: results
+ */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
- dlistint_t *new = malloc(sizeof(dlistint_t));
- /*dlistint_t *temp = *head;*/
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	/*dlistint_t *temp = *head;*/
 
-if (new == NULL)
-return (NULL);
+	if (new == NULL)
+		return (NULL);
 
-new->n = n;
-new->prev = NULL;
-new->next = *head;
-*head = new;
+	if (*head != NULL)
+		(*head)->prev = new;
 
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
 
-if (*head != NULL)
-    (*head)->prev = new;
-
-return (new);
-
+	return (new);
 }
