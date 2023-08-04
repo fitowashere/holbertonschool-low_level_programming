5056930092a382199b4c9b71c3a1fbf48c774142
fitowashere <fito_washere@icloud.com>
2023-08-04 11:16:03 -0400

    betty fix

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
 	dlistint_t *current = head;
-	while (current != NULL) 
+    
+	while (current != NULL)
 	{
 		if (count == index)
 			return (current);
