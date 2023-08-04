d1c54bcec740199d94966f3dffafe5f2a624fa6c
fitowashere <fito_washere@icloud.com>
2023-08-04 15:40:08 -0400

    jonathan fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -36,10 +36,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
     new_node->n = n;
-	new_node->prev = temp->prev;
 	new_node->next = temp->next;
-	temp->next->prev = new_node;
+	new_node->prev = temp;
 	temp->next = new_node;
+	temp = new_node;
 
 	return (new_node);
 
