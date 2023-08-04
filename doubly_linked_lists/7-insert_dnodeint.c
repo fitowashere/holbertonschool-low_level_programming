1a7a5a5f29a4dbee6adf7f76c254eab5b21bae76
fitowashere <fito_washere@icloud.com>
2023-08-04 15:08:01 -0400

    hot fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,27 +10,20 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+    dlistint_t *new_node;
 	dlistint_t *temp = *h;
 
 	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
 	}
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-
+	
 	for (; idx != 1; idx--)
 	{
 		temp = temp->next;
 		if (new_node == NULL)
 			return (NULL);
 
-		idx--;
 	}
 
 	if (temp->next == NULL)
@@ -38,12 +31,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint_end(h, n));
 	}
 
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+		return (NULL);
+
+    new_node->n = n;
 	new_node->prev = temp->prev;
 	new_node->next = temp->next;
 	temp->next->prev = new_node;
 	temp->next = new_node;
-
-
 
 	return (new_node);
 
