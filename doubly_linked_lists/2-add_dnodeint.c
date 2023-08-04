b84a0103d7bc982cd6a4dc853cb4b364b12e09e9
fitowashere <fito_washere@icloud.com>
2023-08-04 08:25:29 -0400

    fixing code

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,9 +13,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
  /*dlistint_t *temp = *head;*/
 
 if (new == NULL)
-{
 return (NULL);
-}
 
 new->n = n;
 new->prev = NULL;
@@ -23,12 +21,8 @@ new->next = *head;
 
 
 if (*head != NULL)
-{
     (*head)->prev = new;
-  /*  temp->prev = new; */
-/*this can also be donde by using (*head) instead of temp to ensure the order*/
-}
-
+    
 return (new);
 
 }
