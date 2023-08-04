3fecf25ab11fc4afe2fb721798292b85315d3747
fitowashere <fito_washere@icloud.com>
2023-08-04 08:50:02 -0400

    fixed betty

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -2,21 +2,21 @@
 
 /**
  * sum_dlistint- gets input
- * @head: stores input 
- * Return: results 
-*/
+ * @head: stores input
+ * Return: results
+ */
 
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
+	int sum = 0;
 
-    if (head == NULL)
-    return (0);
+	if (head == NULL)
+		return (0);
 
-    while (head != NULL)
-    {
-        sum += head -> n ;
-        head = head->next;
-    }
-    return (sum);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
