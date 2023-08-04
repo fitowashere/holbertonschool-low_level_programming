27c0b27fb4a95ac19a9074129e52e53e2cd2f865
fitowashere <fito_washere@icloud.com>
2023-08-04 15:56:20 -0400

    jonathan fixed

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,13 +12,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
     dlistint_t *new_node;
 	dlistint_t *temp = *h;
+    unsigned int i;
 
 	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
 	}
 	
-	for (; idx != 1; idx--)
+	for (i = 0; i < idx - 1; i++)
 	{
 		temp = temp->next;
 		if (temp == NULL)
@@ -38,8 +39,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
     new_node->n = n;
 	new_node->next = temp->next;
 	new_node->prev = temp;
+	temp->next->prev = new_node;
 	temp->next = new_node;
-	temp = new_node;
 
 	return (new_node);
 
