a7ce61e9f4f8b1c7309c28a64fbb1260e2adcb3d
fitowashere <fito_washere@icloud.com>
2023-08-05 17:14:57 -0400

    task 12

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - gets input
+ * Return: results
+ */
+
+int main(void)
+{
+	int i, x;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (x = 0; x < 100; x++)
+		{
+			if (i != x)
+			{
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+				putchar(32);
+				putchar(x / 10 + 48);
+				putchar(x % 10 + 48);
+
+				if (i != 99 || x != 99)
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+
+}
