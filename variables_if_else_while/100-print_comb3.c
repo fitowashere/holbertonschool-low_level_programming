91d8347d499beb36b290876582651dc466c92dfa
fitowashere <fito_washere@icloud.com>
2023-08-05 15:53:26 -0400

    betty fix

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -11,25 +11,20 @@ int main(void)
 {
 	int i, x;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
 		for (x = 0; x < 10; x++)
 		{
 			putchar(i + 48);
 			putchar(x + 48);
 
-			if (x < 10)
+			if (i != 8 || x != 9)
 			{
 				putchar(44);
 				putchar(32);
 			}
 		}
 	}
-	/*if (x == 10)
-	*{
-	*	putchar(49, 48, 48);
-	*}
-    */
 	return (0);
 }
 
