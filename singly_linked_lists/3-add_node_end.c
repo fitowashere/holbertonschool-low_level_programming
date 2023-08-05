e3410d0a869f9a3f93c541b96365a700b8722b0d
fitowashere <fito_washere@icloud.com>
2023-08-04 23:03:28 -0400

    betty fixes

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,34 +9,29 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	unsigned int count = 0;
-	int i;
 
 	list_t *list = malloc(sizeof(list_t));
 	list_t *end_list = malloc(sizeof(list_t));
 
+	if (str == NULL || list == NULL)
+		return (NULL);
+
 	list->str = strdup(str);
-
-	if (str == NULL)
-		return (NULL);
-
-	if (list == NULL)
-		return (NULL);
-
 	if (list->str == NULL)
 	{
 		free(list);
 		return (NULL);
 	}
-    	for (i = 0; list->str[i] != '\0'; i++)
-	{
-		count++;
-	}
 
-	list->len = count;
+	list->len = strlen(str);
+	list->next = NULL;
 
 	if (*head == NULL)
+	{
 		*head = list;
+		return (list);
+	}
+
 	else
 	{
 		end_list = *head;
