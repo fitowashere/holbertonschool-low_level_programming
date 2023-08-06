3e0ec4744ebdfb5e01106b673e27d469d5326d22
fitowashere <fito_washere@icloud.com>
2023-08-05 20:03:07 -0400

    TASK 13

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- gets input
+ * Return: results
+*/
+
+int main(void)
+{
+int i , results;
+
+for (i = 0; i < 1024; i++)
+{
+if ( i % 3 == 0 || i % 5 == 0)
+{
+results += i;
+}
+}
+ printf("%d\n", results);
+ return (0);
+
+}
