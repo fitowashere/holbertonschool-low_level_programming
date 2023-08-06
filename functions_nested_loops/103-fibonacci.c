763e2ebab47aa1535cc071bae1fbb4d2da1d3c70
fitowashere <fito_washere@icloud.com>
2023-08-05 23:20:27 -0400

    task 15

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- gets input
+ * Return: results
+*/
+
+int main(void)
+{
+     int a = 1, b = 2, temp;
+    int sum = 2; // Start with the initial sum of 2 (for the second term)
+
+    printf("%d, %d, ", a, b);
+
+    while (sum + b <= 4000000) {
+        temp = a + b;
+        sum += temp;
+        printf("%d, ", temp);
+        a = b;
+        b = temp;
+    }
+    return (0);
+}
